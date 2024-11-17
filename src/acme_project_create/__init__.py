82fb5711d041d5b5767b0d662bb03e30f57e9b92
Stan Guner <guner.stan@gmail.com>
2024-11-17 22:10:12 +0000

    use .env file by default

--- src/acme_project_create/__init__.py
+++ src/acme_project_create/__init__.py
@@ -12,6 +12,10 @@ from .main import (
 
 import logging
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(pathname)s | %(name)s | func: %(funcName)s:%(lineno)s | %(levelname)s | %(message)s",
